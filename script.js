3fc083945b6906ce0eb0c6db514651923eec0b87
Carolis <89231697+carolisc@users.noreply.github.com>
2025-07-13 03:56:45 -0300

    new quotes

--- script.js
+++ script.js
@@ -13,7 +13,112 @@ const quotes = [
       quote: '"I am stupid. I am stupid."',
       correct: 'Charles Leclerc',
       options: ['Carlos Sainz', 'Charles Leclerc', 'Daniel Ricciardo']
-    }
+    },
+    {
+        quote: '"No, Mikey! No! This is so not right!"',
+        correct: 'Toto Wolff',
+        options: ['Sebastian Vettel', 'Toto Wolff', 'Lewis Hamilton']
+      },
+      {
+        quote: '"Honestly, what are we doing here?"',
+        correct: 'Sebastian Vettel',
+        options: ['Max Verstappen', 'Sebastian Vettel', 'Fernando Alonso']
+      },
+      {
+        quote: '"I am stupid. I am stupid."',
+        correct: 'Charles Leclerc',
+        options: ['Carlos Sainz', 'Charles Leclerc', 'Daniel Ricciardo']
+      },
+      {
+        quote: '"Leave me alone, I know what I’m doing."',
+        correct: 'Kimi Räikkönen',
+        options: ['Kimi Räikkönen', 'Valtteri Bottas', 'Lewis Hamilton']
+      },
+      {
+        quote: '"Fernando is faster than you."',
+        correct: 'Rob Smedley',
+        options: ['Rob Smedley', 'Felipe Massa', 'Fernando Alonso']
+      },
+      {
+        quote: '"It’s called a motor race, Toto."',
+        correct: 'Michael Masi',
+        options: ['Christian Horner', 'Michael Masi', 'Toto Wolff']
+      },
+      {
+        quote: '"I want to win, but not like this."',
+        correct: 'Lewis Hamilton',
+        options: ['Lewis Hamilton', 'Sebastian Vettel', 'George Russell']
+      },
+      {
+        quote: '"I’m not here to finish second or third."',
+        correct: 'Max Verstappen',
+        options: ['Max Verstappen', 'Nico Rosberg', 'Lando Norris']
+      },
+      {
+        quote: '"F***ing Fernando, this guy is a joke."',
+        correct: 'Kevin Magnussen',
+        options: ['Lewis Hamilton', 'Kevin Magnussen', 'Yuki Tsunoda']
+      },
+      {
+        quote: '"They gave me black and orange again, what a joke."',
+        correct: 'Haas Engineer (sobre Magnussen)',
+        options: ['Magnussen', 'Haas Engineer', 'Guenther Steiner']
+      },
+      {
+        quote: '"We were looking like clowns out there."',
+        correct: 'Carlos Sainz',
+        options: ['Carlos Sainz', 'Charles Leclerc', 'George Russell']
+      },
+      {
+        quote: '"It’s not my job to decide the penalties."',
+        correct: 'Sebastian Vettel',
+        options: ['Max Verstappen', 'Sebastian Vettel', 'Lance Stroll']
+      },
+      {
+        quote: '"Tell Esteban to defend like a lion!"',
+        correct: 'Alonso',
+        options: ['Gasly', 'Alonso', 'Otmar Szafnauer']
+      },
+      {
+        quote: '"More power, baby!"',
+        correct: 'Daniel Ricciardo',
+        options: ['Carlos Sainz', 'Lando Norris', 'Daniel Ricciardo']
+      },
+      {
+        quote: '"Box, box, box!"',
+        correct: 'Engineer (genérico)',
+        options: ['Engineer', 'Lewis Hamilton', 'Toto Wolff']
+      },
+      {
+        quote: '"It’s lights out and away we go!"',
+        correct: 'David Croft',
+        options: ['David Croft', 'Martin Brundle', 'James Hunt']
+      },
+      {
+        quote: '"For what?! He turned into me!"',
+        correct: 'Sebastian Vettel',
+        options: ['Lewis Hamilton', 'Sebastian Vettel', 'Valtteri Bottas']
+      },
+      {
+        quote: '"OK, I need more silence on the radio."',
+        correct: 'Max Verstappen',
+        options: ['Sergio Pérez', 'Max Verstappen', 'Kimi Räikkönen']
+      },
+      {
+        quote: '"Guenther, this car is a f***ing joke!"',
+        correct: 'Romain Grosjean',
+        options: ['Mick Schumacher', 'Romain Grosjean', 'Kevin Magnussen']
+      },
+      {
+        quote: '"We look like amateurs."',
+        correct: 'George Russell',
+        options: ['George Russell', 'Esteban Ocon', 'Lance Stroll']
+      },
+      {
+        quote: '"If he doesn’t move, I will crash into him!"',
+        correct: 'Yuki Tsunoda',
+        options: ['Yuki Tsunoda', 'Fernando Alonso', 'Charles Leclerc']
+      }
   ];
   
   let current = 0;
